149c71d72a8e21eaf8f8be6b4f083ecc26ba810d
kassy708 <kassysmilingtogether@gmail.com>
2014-11-30 14:29:45 +0900

    キーボードで攻撃追加

--- Snowball fight/Assets/Scripts/PlayerController.cs
+++ Snowball fight/Assets/Scripts/PlayerController.cs
@@ -38,14 +38,8 @@ public class PlayerController : MonoBehaviour {
         GameObject camera = GameObject.FindWithTag("MainCamera");
         OVRCameraRig ovrCameraRig = camera.GetComponent<OVRCameraRig>();
         //Vector3 vec = ovrCameraRig.centerEyeAnchor.forward * ballSpeed;
-        Debug.Log(ovrCameraRig.centerEyeAnchor.forward);
+        //Debug.Log(ovrCameraRig.centerEyeAnchor.forward);
 
-        /*余裕があれば
-        if (VirtualInput.instance.CheckInput())
-        {
-
-        }
-         * */
         
         HandModel[] physicsHands = handController.GetAllPhysicsHands();
         for (int i = 0; i < physicsHands.Length; i++) {
@@ -71,40 +65,49 @@ public class PlayerController : MonoBehaviour {
             else throwState[handID] = ThrowState.None;
         }
 
+
+        // Fire1が押された！(押され続けは検知されない)
+        if (Input.GetButtonDown("Fire1")) {
+            this.ThrowBallForward();
+        }
 	}
     void ThrowBall(HandModel handModel) {
-
-        Vector3 pos = handModel.GetPalmPosition();
-
-        GameObject centerEyeAnchor = GameObject.Find("CenterEyeAnchor");
-        Vector3 vec = centerEyeAnchor.transform.forward * ballSpeed;
-
-        pos += centerEyeAnchor.transform.forward;
-
+        
         // プレハブからボールを生成
-        GameObject newBall = (GameObject)Instantiate(ball, pos, new Quaternion());
-        // ballControllerを取得
-        BallController ballController = newBall.GetComponent<BallController>();
+        GameObject newBall = (GameObject)Instantiate(ball, handModel.GetPalmPosition(), new Quaternion());
 
         RigidHand rigidHand = handModel.GetComponent<RigidHand>();
 
-        vec = rigidHand.palm.rigidbody.velocity;
+        // 手と球の当たり判定を消す
+        Leap.Utils.IgnoreCollisions(rigidHand.gameObject, newBall);
+
+        // 腕の速度を球に与える
+        Vector3 vec = rigidHand.palm.rigidbody.velocity;
         vec.y += 2; // ちょっと上に投げる
         vec *= ballSpeed;
 
+        // ballControllerを取得
+        BallController ballController = newBall.GetComponent<BallController>();
         // 速度を設定
         ballController.SetVec(vec);
 
 
-        // 視線方向に球を投げる
-        //GameObject camera = GameObject.FindWithTag("MainCamera");
-        //OVRCameraRig ovrCameraRig = camera.GetComponent<OVRCameraRig>();
-        //Vector3 vec = ovrCameraRig.centerEyeAnchor.forward * ballSpeed;
-        ////Debug.Log(ovrCameraRig.centerEyeAnchor.forward);
-        //ballController.SetVec(ovrCameraRig.centerEyeAnchor.forward * ballSpeed);
+        
+    }
 
-        //ballController.SetVec(vec);
-        
-        
+    void ThrowBallForward() {
+         //視線方向に球を投げる
+        GameObject camera = GameObject.FindWithTag("MainCamera");
+        OVRCameraRig ovrCameraRig = camera.GetComponent<OVRCameraRig>();
+
+        GameObject firingPort = GameObject.FindWithTag("FiringPort");
+        // プレハブからボールを生成
+        GameObject newBall = (GameObject)Instantiate(ball, firingPort.transform.position, new Quaternion());
+
+        Vector3 vec = ovrCameraRig.centerEyeAnchor.forward * ballSpeed * 2;
+        vec.y += 2; // ちょっと上に投げる
+        // ballControllerを取得
+        BallController ballController = newBall.GetComponent<BallController>();
+        ballController.SetVec(vec);
     }
 }
